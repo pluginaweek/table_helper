852fa956a7cd01f5867e53a19f3928c2c12da9a7
Marc-Andre Lafortune <github@marc-andre.ca>
2012-05-21 14:07:50 -0400

    Add html_safety test

--- test/helpers/table_helper_test.rb
+++ test/helpers/table_helper_test.rb
@@ -46,4 +46,42 @@ class TableHelperTest < ActionView::TestCase
     end_str
     assert_html_equal expected, html
   end
+
+  def test_should_be_html_safe_in_rails_3
+    html = collection_table(['<a&b>', '<b>ok!</b>'.html_safe], Post) do |t|
+      t.header :title
+      t.rows.each do |row, post_title, index|
+        row.title post_title
+      end
+      t.footer :total, '<c&d>'
+      t.footer :grand_total, '<b>ok too!</b>'.html_safe
+    end
+
+    assert html.html_safe?
+
+    expected = <<-end_str
+      <table cellpadding="0" cellspacing="0" class="posts ui-collection">
+        <thead>
+          <tr>
+            <th class="post-title" scope="col">Title</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="post ui-collection-result">
+            <td class="post-title">&lt;a&amp;b&gt;</td>
+          </tr>
+          <tr class="post ui-collection-result">
+            <td class="post-title"><b>ok!</b></td>
+          </tr>
+        </tbody>
+        <tfoot>
+          <tr>
+            <td class="post-total">&lt;c&amp;d&gt;</td>
+            <td class="post-grand_total"><b>ok too!</b></td>
+          </tr>
+        </tfoot>
+      </table>
+    end_str
+    assert_html_equal expected, html
+  end if "".respond_to?(:html_safe?)
 end
